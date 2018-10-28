8413c5b5be166b8ca743e965ae72c8f64c17483b
Vishesh Choudhary <vishachoudhary@gmail.com>
2018-10-28 14:11:41 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Weather_ForcastxD
+# Weather Forecast  App in Angular JS.
+
+This is a simple weather app created to practice AngularJs, which can be used to search weather details of a small town or city upto a maximum of 5 days.
+It shows the day time, night time, minimum temperature along with the maximum temperature for each day offering maximum convenience to the end user. 
+
---
+++ app.js
@@ -0,0 +1,65 @@
+var wspa = angular.module('wspa',['ngRoute','ngResource']);
+
+wspa.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/forecast',{
+        templateUrl: 'pages/forecast.html',
+        controller: 'weatherController'
+    })
+    .when('/forecast/:daysNo',{
+        templateUrl:'pages/forecast.html',
+        controller: 'weatherController'
+    })
+});
+
+wspa.service('nameService',function(){
+    this.city = "";
+});
+
+wspa.controller('mainController', ['$scope','nameService', function ($scope,nameService) {
+    $scope.cityName = nameService.city;
+    $scope.$watch('cityName',function(){
+          nameService.city = $scope.cityName;
+      });
+}]);
+
+wspa.controller('weatherController',['$scope','$resource','$log','$routeParams','nameService', function ($scope,$resource,$log,$routeParams,nameService) {
+    var dayRange = [];
+    for(var j = 1; j < 6 ; j++){
+        dayRange.push(j);
+    }
+    $scope.dayRange = dayRange;
+
+    $scope.cityName = nameService.city;
+    $scope.daysNo = $routeParams.daysNo || 1;
+
+    $scope.forecastApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, {get: { method: "JSONP"}});
+    $scope.forecastResult = $scope.forecastApi.get({ q: $scope.cityName, cnt:$scope.daysNo , appid:'cf8ca3df282c603789ae4d2a8506d06a'});
+    
+    $scope.convertToCelcius = function(degK) {
+        return  Math.round(degK - 273.15) ;
+    }
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000); 
+    }
+    
+}]);
+
+wspa.directive('wspaResult',function(){
+    return {
+        restrict: 'AE',
+        templateUrl: 'directives/wspa_result.html',
+        replace:true,
+        scope:{
+            forecast: '=',
+            convertToTempUnit: '&',
+            convertToDate: '&',
+            dateFormat: '@',
+        }
+    }
+});
+
